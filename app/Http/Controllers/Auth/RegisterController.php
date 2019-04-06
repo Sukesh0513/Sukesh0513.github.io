a13299c9b8621aab97badc350024ea99d24dbbc7
Sukesh0513 <48705326+Sukesh0513@users.noreply.github.com>
2019-04-06 03:22:06 +0200

    First upload

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+use Illuminate\Http\Request;
+use App\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Storage;
+
+class RegisterController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Register Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users as well as their
+    | validation and creation. By default this controller uses a trait to
+    | provide this functionality without requiring any additional code.
+    |
+    */
+
+    use RegistersUsers;
+
+    /**
+     * Where to redirect users after registration.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => 'required|string|max:255',
+            'mobile'=>'required',
+            'img'=>'required',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+            'user_type'=>'required',
+        ]);
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return \App\User
+     */
+    protected function create(array $data)
+    { 
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'mobile'=>$data['mobile'],
+            'img'=>$data['img'],
+            'user_type'=>$data['user_type'],
+            'password' => Hash::make($data['password']),
+            
+        ]);
+    }
+    
+    public function signUp(Request $req){  
+        if($req->isMethod('post')){  
+            
+            $data=$req->all();
+           $usr= User::where('email','=',$data['email'])->first();
+           if(!$usr){               
+                $path = Storage::putFile('uploads', $req->file('img'));
+                $data['img']=$path;
+                // echo "eirhviuerwerfvub";die();
+                $this->create($data);           
+               return redirect('userlogin');
+               // echo "heyy Here!!!";
+            }
+           else{
+               
+               if($data['user_type']=='user'){
+                    return view('usersignup');
+               }
+               if($data['user_type']=='vendor')
+                {
+                    return view('signup');
+                }
+                $req->session()->flash('message','use exsists with this email');
+            }
+        }
+        if($req->isMethod('get')){
+            return view('signup');
+        }
+
+    }
+}
